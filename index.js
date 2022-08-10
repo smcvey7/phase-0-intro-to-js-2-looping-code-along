d5836da134ac62db49de9383e6e9422a3112bcbf
Steven <smcvey1210@gmail.com>
2022-08-10 10:41:41 -0700

    commit tree 1675c9fa74d6f8d7c91cf455a632578b3d2d7dda

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(list, gift){
+    let messages = []
+    for (let i=0; i<list.length; i++){
+        messages.push(`Thank you, ${list[i]}, for the wonderful ${gift} gift!`)
+    }
+    return messages
+}
+
+function countDown(num){
+    for (let i = num; i > -1; i--){
+        console.log(i)
+    }
+}
